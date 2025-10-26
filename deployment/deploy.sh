2b44e4dcb5516467442d93def71599dfc9e657ce
Youssef ElMountahi <yusuf.one@outlook.com>
2025-10-26 19:07:18 +0100

    fix options-ssl-nginx.conf

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -31,6 +31,16 @@ docker-compose run --rm -p 80:80 --entrypoint "\
 
 echo -e "${GREEN}Certificate obtained successfully! ${NC}\n\n"
 
+echo -e "${YELLOW}Downloading recommended TLS parameters...${NC}\n\n"
+mkdir -p ./tmp-certbot
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "./tmp-certbot/options-ssl-nginx.conf"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "./tmp-certbot/ssl-dhparams.pem"
+
+echo -e "${YELLOW}Copying TLS parameters to certbot volume...${NC}\n\n"
+docker cp ./tmp-certbot/options-ssl-nginx.conf certbot:/etc/letsencrypt/options-ssl-nginx.conf
+docker cp ./tmp-certbot/ssl-dhparams.pem certbot:/etc/letsencrypt/ssl-dhparams.pem
+rm -rf ./tmp-certbot
+
 echo -e "${YELLOW}Restarting all services...${NC}\n\n"
 docker-compose down
 docker-compose up -d
